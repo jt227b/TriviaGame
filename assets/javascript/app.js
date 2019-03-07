a81db6bd7314096a04871126fb3ffbee9067d13f
Jose Torres <jt227b@gmail.com>
2019-03-07 00:07:24 -0600

    updated input JS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -30,3 +30,22 @@ function countdown(){
     }
 }
 
+function startCountdown(){
+    setInterval(countdown, 1000);
+}
+
+function timeUp(){
+    var Q1 = $('input:radio[name="q1"]:checked').val();
+    var Q2 = $('input:radio[name="q2"]:checked').val();
+    var Q3 = $('input:radio[name="q3"]:checked').val();
+    var Q4 = $('input:radio[name="q4"]:checked').val();
+    var Q5 = $('input:radio[name="q5"]:checked').val();
+    var Q6 = $('input:radio[name="q6"]:checked').val();
+    var Q7 = $('input:radio[name="q7"]:checked').val();
+    var Q8 = $('input:radio[name="q8"]:checked').val();
+    var Q9 = $('input:radio[name="q9"]:checked').val();
+    var Q10 = $('input:radio[name="q10"]:checked').val();
+
+    
+
+}
